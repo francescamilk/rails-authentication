e79fc7a5240759006d8a623d0cc04af5e62df496
Francesca Santoriello <f.santoriello@hotmail.com>
2023-11-20 10:14:21 +0100

    rails new

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,17 @@
 class PagesController < ApplicationController
+  skip_before_action :authenticate_user!, only: :home
+
+  before_action :some_method
+
   def home
+  end
+
+  def about
+  end
+
+  private 
+
+  def some_method
+    age = 28
   end
 end
